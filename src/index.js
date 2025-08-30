d6adcd7dfaaeb7107f110ebffae893602288ca92
yhakku <karginyura13@gmail.com>
2025-08-30 15:42:06 +0300

    feat: implement reverse function for numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const absN = Math.abs(n);
+  const res = String(absN).split('').reverse().join('');
+
+  return Number(res);
 };
